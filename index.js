c55e451fa80d62a85c6a050c21d9dd78bf9abd23
Thomas-VWD <thomas.verwaerde@gmail.com>
2023-03-28 11:22:31 +0200

    part one == OK

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter.js")
+
+const heracles = new Fighter("Heracles", 20, 6);
+console.log(heracles)
+const nemanLion = new Fighter("Nemean Lion", 11, 13);
+console.log(nemanLion)
+
